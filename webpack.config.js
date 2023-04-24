6ae248e4571f874be24be41ade9a279a13ac174f
g-shetti <gangadhar@nichesolv.com>
2023-04-24 17:36:26 +0530

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "/src/index.js",
-  output: { path: path.resolve(__dirname, "dist") },
+  output: { path: path.resolve(__dirname, "dist"), clean: true },
   module: {
     rules: [
       {
